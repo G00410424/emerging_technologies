31fe15ec15df22e368581300da56ba3eb97552ab
Estevan Fernandez <123736252+G00410424@users.noreply.github.com>
2024-10-24 14:01:52 +0000

    Addedd books to this VM Codespace so I can use. More code was added.

--- trigrams.py
+++ trigrams.py
@@ -12,3 +12,18 @@ def clean_text(text):
     # Convert to uppercase
     cleaned_text = cleaned_text.upper()
     return cleaned_text
+
+# Example file path
+file_path = r'C:\Users\estev\OneDrive - Atlantic TU\4. Fourth Year\First Semester\Emerging Technologies\Task1\Books\book1_paris'
+
+text = read_file(file_path)
+cleaned = clean_text(text)
+print(cleaned[:100])  # Print the first 100 characters to check
+
+def remove_preamble_postamble(text):
+    start_index = text.find("START OF THIS PROJECT GUTENBERG")  # Find the start of the actual content
+    end_index = text.find("END OF THIS PROJECT GUTENBERG")      # Find the end of the actual content
+
+    if start_index != -1 and end_index != -1:
+        text = text[start_index:end_index]
+    return text
